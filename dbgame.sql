afdb168a42719e4f34c4578931df3ab155a9d626
lanceferr <lanceferrer1122@gmail.com>
2024-11-23 20:01:00 +0800

    to match schema

--- dbgame.sql
+++ dbgame.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `branches`
 --
-
+USE attempt;
 DROP TABLE IF EXISTS `branches`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
