9fd5825599049a5443003e08d434d009122623ef
LeeH4m-A <165268004+LeeH4m-A@users.noreply.github.com>
2024-11-25 12:01:36 +0800

    Update dbvideogame.sql

--- dbvideogame.sql
+++ dbvideogame.sql
@@ -167,7 +167,8 @@ CREATE TABLE `receipts` (
   KEY `product_id_purchased` (`product_id_purchased`),
   KEY `branch_id` (`branch_id`),
   CONSTRAINT `receipts_ibfk_1` FOREIGN KEY (`product_id_purchased`) REFERENCES `products` (`product_id`) ON DELETE SET NULL,
-  CONSTRAINT `receipts_ibfk_2` FOREIGN KEY (`branch_id`) REFERENCES `branches` (`branch_id`) ON DELETE SET NULL
+  CONSTRAINT `receipts_ibfk_2` FOREIGN KEY (`branch_id`) REFERENCES `branches` (`branch_id`) ON DELETE SET NULL,
+  CONSTRAINT `suppliers_ibfk_3` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -196,8 +197,7 @@ CREATE TABLE `suppliers` (
   PRIMARY KEY (`supplier_id`,`product_id`),
   KEY `product_id` (`product_id`),
   CONSTRAINT `suppliers_ibfk_1` FOREIGN KEY (`supplier_id`) REFERENCES `branches` (`branch_id`) ON DELETE CASCADE,
-  CONSTRAINT `suppliers_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`) ON DELETE CASCADE,
-  CONSTRAINT `suppliers_ibfk_3` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`) ON DELETE CASCADE
+  CONSTRAINT `suppliers_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
